fa518b12a037afd4e14efcc17e0574ad2735cf57
Briana Bennett <bri.bennett89@gmail.com>
2017-08-09 15:41:13 -0700

    can't render templates

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 
 def index(request):
-	response = "Hello, I am your first request!"
-	return HttpResponse(response)
+	return render(request, 'first_app/landing.html')
