4ef61bfbbe498e0971192a33106a277e697ad215
Briana Bennett <bri.bennett89@gmail.com>
2017-08-09 15:49:27 -0700

    corrected settings.py, all is good in the world again

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps',
+    'apps.first_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
