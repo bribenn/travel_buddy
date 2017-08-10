c166f798f839342a703bbe796e29542576b983aa
Briana Bennett <bri.bennett89@gmail.com>
2017-08-10 00:41:38 -0700

    working on models

--- apps/first_app/models.py
+++ apps/first_app/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import re
 
 # Create your models here.
+class User(models.Model):
+	first_name = models.CharField(max_length = 45)
+	last_name = models.CharField(max_length = 45)
+	email = models.CharField(max_length = 100)
+	password = models.CharField(max_length = 255)
+	trips = models.ManyToManyField(Trip, related_name="trips", default=None)
+	friends = models.ManyToManyField("self")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now_add = True)
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -4,3 +4,28 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
 	return render(request, 'first_app/landing.html')
+
+def register(request):
+	check = User.objects.validateUser(request.POST)
+	if request.method != 'POST':
+		return redirect('/')
+	if check[0] == False:
+		for error in check[1]:
+			messages.add_message(request, messages.INFO, error, extra_tags="registration")
+			return redirect('/')
+	if check[0] == True:
+		#has password
+		hashed_pw = bcrypt.hashpw(request.POST.get('password').encode(), bcrypt.gensalt())
+
+		#create user
+		user = User.objects.create(
+			username = request.POST.get('username'),
+			email = request.POST.get('email'),	
+			password = hashed_pw,
+		)
+
+
+		#add user to session, logging them in
+		request.session['user_id'] = user.id
+		#route to profile form page
+		return redirect('/')
